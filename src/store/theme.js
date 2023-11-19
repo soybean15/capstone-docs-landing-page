3f29de218991ac453e74ef7dc5fcc5993b6e6ced
soybean15 <marlonpadilla1593@gmail.com>
2023-11-19 18:55:59 +0800

    fix theme

--- src/store/theme.js
+++ src/store/theme.js
@@ -5,11 +5,13 @@ import { ref } from "vue";
 export const useThemeStore = defineStore('theme',()=>{
 
 
-    const isDark = ref(localStorage.getItem('isDark')==null ? true: false)
+    const isDark = ref(localStorage.getItem('isDark')==null ? true: localStorage.getItem('isDark'))
 
     const toggleDarkMode = ()=>{
         isDark.value= !isDark.value
         localStorage.setItem('isDark',isDark.value)
+
+        
 
     }
 
