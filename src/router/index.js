8a0021987f674483dc8b64ae3e7b7b317dd28933
soybean15 <marlonpadilla1593@gmail.com>
2023-12-10 20:01:40 +0800

    modified routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter,  createWebHistory } from 'vue-router'
+import { createRouter,  createWebHashHistory,   } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const routes = [
@@ -23,7 +23,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
   scrollBehavior: function(to, from, savedPosition) {
     if (to.hash) {
